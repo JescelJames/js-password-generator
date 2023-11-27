1c7d8bfb97f4402e4d3636f0bbf5e3c9b569036a
Jescel James <jesceljames@gmail.com>
2023-11-27 18:14:58 -0500

    added variables

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 // DATA 
+var lowercaseAll = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseAll = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbersAll = '0123456789';
+var symbolsAll = "!@#$%^&*()_+=-[]\{}|;':",./<>?~``;
 
 
 // FUNCTIONS
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 
 //USER INTERACTION
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // INITIALIZATION
 
@@ -18,14 +29,8 @@ var generateBtn = document.querySelector("#generate");
 // Assignment Code
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
