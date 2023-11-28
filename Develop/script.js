3c4674b7775560f8b4f4aa055b2febc54ef62087
Jescel James <jesceljames@gmail.com>
2023-11-27 19:05:04 -0500

    added function generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -5,19 +5,42 @@ var generateBtn = window.document.querySelector("#generate"); // <button>
 var lowercaseAll = 'abcdefghijklmnopqrstuvwxyz';
 var uppercaseAll = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbersAll = '0123456789';
-var symbolsAll = "!@#$%^&*()_+=-[]\{}|;':",./<>?~``;
+var symbolsAll = '!@#$%^&*()_+{}:"<>?|[];\',./`~';
+var allowedCharacters = " ";
+
 
 
 // FUNCTIONS // Write password to the #password input
+function generatePassword() {
+alert("Hi!");
+prompt("Would you like to generate a password: ");
+}
+
+generatePassword();
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 
+function hiToUser() {
+  
+}
+hiToUser();
+
+
+function askUser() {
+  
+}
+askUser();
+
+
+
+
 //USER INTERACTION // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 
