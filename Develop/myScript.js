5b329acaed5d826d580a581b712a0167d3c05d6e
Jescel James <jesceljames@gmail.com>
2023-11-29 10:05:22 -0500

    update on if validation password lenght prompt

--- Develop/myScript.js
+++ Develop/myScript.js
@@ -4,31 +4,40 @@ var generateButton = document.getElementById('generateButton'); //<button id="ge
 //DATA 
 
 
-//FUNCTIONS // Write password to the #password input
+//FUNCTIONS 
 function generatePassword() { 
-    var lowercaseAll = 'abcdefghijklmnopqrstuvwxyz';
-    var uppercaseAll = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var numbersAll = '0123456789';
-    var symbolsAll = '!@#$%^&*()_+{}:"<>?|[];\',./`~';
-    
-    
     //Prompt for length
-    var passwordLength = prompt("Enter the desired length of at least 8 characters and no more than 128 characters");
-    
-    
-    //Validation of password length
-    if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
-      alert("Please choose password length between 8 and 128 characters!")
-    }
-   
+    var passwordLength;
 
+    while (true) {
+        passwordLength = prompt("Enter the desired length of at least 8 characters and no more than 128 characters");
+    
+        // Convert passwordLength to a number
+        passwordLength = parseInt(passwordLength, 10);
+    
+        if (passwordLength >= 8 && passwordLength <= 128) {
+            // If the length is valid, exit the loop
+            break;
+        } else {
+            // Alert the user and repeat the loop
+            alert("Please choose a password length between 8 and 128 characters!");
+        }
+    }
+
+
+    // var passwordLength = prompt("Enter the desired length of at least 8 characters and no more than 128 characters");
+    // if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
+    //   alert("Please choose password length between 8 and 128 characters!")
+    // } else {
+
+    // }
+   
+    
     //Confirmation of character types
     var useLowercase = confirm("Do you want to include lowercase characters?");
-    var useUppercase = confirm("Do you want to include uppercase characters?");
-    var useNumbers = confirm("Do you want to include numbers?");
-    var useSymbols = confirm("Do you want to include symbols?");
-    
-    
+    var useUppercase = confirm("How about uppercase characters? Do you want to include them?");
+    var useNumbers = confirm("Would you like to have numbers in your password?");
+    var useSymbols = confirm("Lastly, do you want to include symbols?");
     //Validation 
     if (!useLowercase && !useUppercase && !useNumbers && !useSymbols) {
       alert("At least one character type must be selected.");
@@ -36,26 +45,18 @@ function generatePassword() {
     }
     
 
+    //Possible characters
+    var lowercaseAll = 'abcdefghijklmnopqrstuvwxyz';
+    var uppercaseAll = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numbersAll = '0123456789';
+    var symbolsAll = '!@#$%^&*()_+{}:"<>?|[];\',./`~';
+
     //Create a string of possible characters
-
     var allCharacters = lowercaseAll + uppercaseAll + numbersAll + symbolsAll;
-        // var possibleCharacters = '';
-        // if (useLowercase) possibleCharacters += lowercase;
-        // if (useUppercase) possibleCharacters += uppercase;
-        // if (useNumbers) possibleCharacters += numbers;
-        // if (useSymbols) possibleCharacters += symbols;
-    
-    
-    // if (useLowercase) {
-      //   possibleCharacters += lowercase;
-      //   }
-
-
+    //Randomized allCharacters.length
     for (var i = 0; i < length; i++) {
-      //Randomized allCharacters.length
-      var randomCharacters = Math.floor(Math.random() * allCharacters.length);
-      
-      userPassword += allCharacters[randomCharacters];
+        var randomCharacters = Math.floor(Math.random() * allCharacters.length);
+        userPassword += allCharacters[randomCharacters];
     }
 
     return userPassword;
@@ -65,9 +66,7 @@ function generatePassword() {
 
 function createPassword() {
     var newPassword = generatePassword();
-  
     var passwordText = document.getElementById('passwordTextArea');
-  
     passwordText.value = newPassword;
 }
 
