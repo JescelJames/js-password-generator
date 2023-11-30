8f30da69bbe2b2b4b7939bd07eec6a5c061aac5a
Jescel James <jesceljames@gmail.com>
2023-11-29 21:21:42 -0500

    in the process of clearing text area when button is pressed

--- Develop/myScript.js
+++ Develop/myScript.js
@@ -1,18 +1,58 @@
 // DEPENDENCIES 
 var generateButton = document.getElementById('generateButton'); //<button id="generateButton" class="btn">
-
+// var passwordText = document.getElementById('passwordTextArea');
 
 //DATA 
+// function clearText() {
+ 
+// passwordText.value = "";
+// }
+
+//     generateButton.addEventListener('click', clearText());
 
 
-//FUNCTIONS 
-function generatePassword() { 
+    // Clear the existing password
+    
+    //  document.getElementById("generateButton").addEventListener("click", function() {
+    //     // Clear the text area
+    //     var textArea = document.getElementById("passwordTextArea");
+    //     textArea.value = '';
+ 
+
+    //     // return textArea;
+    
+    // });
+
+
+
+//FUNCTIONS     
+
+
+
+
+ 
+function generatePassword() {
+     
+    
+
+    
+    
+    
+    
     //Prompt for length
-    var passwordLength;
     while (true) {
         passwordLength = prompt("Enter the desired length of at least 8 characters and no more than 128 characters");
+    
+        // Check if the prompt was cancelled
+        if (passwordLength === null) {
+            // Optional: Refresh the page or handle the cancellation appropriately
+            // window.location.reload(); // Uncomment this to refresh the page
+            return; // Or break out of the loop
+        }
+    
         // Convert passwordLength to a number
         passwordLength = parseInt(passwordLength, 10);
+    
         if (passwordLength >= 8 && passwordLength <= 128) {
             // If the length is valid, exit the loop
             break;
@@ -28,9 +68,7 @@ function generatePassword() {
     var useUppercase = confirm("How about UPPERCASE characters? Do you want to include them?");
     var useNumbers = confirm("Would you like to have NUMBERS in your password?");
     var useSymbols = confirm("Lastly, do you want to include SYMBOLS?");
- 
-
-    // If Validations
+    // Validations
     if (!useLowercase && !useUppercase && !useNumbers && !useSymbols) {
       alert("At least one character type must be selected.");
       return '';
@@ -57,6 +95,7 @@ function generatePassword() {
     }
 
     return userPassword;
+
 }
 
 
@@ -64,6 +103,7 @@ function createPassword() {
     var newPassword = generatePassword();
     var passwordText = document.getElementById('passwordTextArea');
     passwordText.value = newPassword;
+  
 }
 
 
